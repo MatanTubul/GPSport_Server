d297bbbe36e2433aab5ce220e3440bb16623be8f
Nir Bercovitz <nirbercovic@gmail.com>
2015-08-16 15:53:40 +0300

    Noon commit and push 16/8. working on register.

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: matant
- * Date: 8/9/2015
- * Time: 6:35 PM
+ * User: Nir B
+ * Date: 16/08/2015
+ * Time: 15:24
  */
+
+error_reporting(0);
+require 'connection.php';
+
+$tag = $_POST['tag'];
+if ($tag == "login")
+{
+    include 'login.php';
+    $login = new Login();
+    echo ($login->dataProcess($dblink));
+}
+else if ($tag == "signup")
+  {
+      include 'register.php';
+      $register = new register();
+      echo ($register->dataProcess ($dblink));
+  }
+
+//else signup,forgotpassword and other...
+
+$dblink->close();
---
+++ response_process.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nir B
+ * Date: 16/08/2015
+ * Time: 15:18
+ */
+
+interface ResponseProcess
+{
+    public function dataProcess($dblink);
+
+}
+
