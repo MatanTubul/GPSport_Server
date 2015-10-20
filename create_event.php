1b85a3e1383981fa51b65f885914ae25cfce734d
matant <matant86@gmail.com>
2015-10-20 17:03:11 +0300

    update  20/10- invite users: -insert new event to attending table

--- create_event.php
+++ create_event.php
@@ -26,6 +26,7 @@ class CreateEvent implements ResponseProcess {
         $max_p = $_POST["max_participants"];
         $sched = $_POST["scheduled"];
         $gen = $_POST["gender"];
+        $min_age = $_POST["minAge"];
 
         $query = "SELECT * FROM event WHERE (event.longtitude = '$lon' AND event.latitude = '$lat')
                 AND event.event_date = '$date' And ((event.start_time BETWEEN '$s_time' AND '$e_time') OR (event.end_time BETWEEN '$s_time' AND '$e_time'))";
@@ -47,12 +48,63 @@ class CreateEvent implements ResponseProcess {
             if ($no_of_rows < 1) {
                 $output["flag"] = "success";
                 $output["msg"] = "insert event";
-                $result = mysqli_query($dblink, "INSERT into event(kind_of_sport,event_date,start_time,end_time,longtitude,latitude,private,gender,max_participants,scheduled,event_status)
-             VALUES ('$sport','$date','$s_time','$e_time','$lon','$lat','$event_type','$gen','$max_p','$sched','1')") or die (mysqli_error($dblink));
+
+                $result = mysqli_query($dblink, "INSERT into event(kind_of_sport,event_date,start_time,end_time,longtitude,latitude,private,gender,min_age,max_participants,scheduled,event_status)
+             VALUES ('$sport','$date','$s_time','$e_time','$lon','$lat','$event_type','$gen','$min_age','$max_p','$sched','1')") or die (mysqli_error($dblink));
                 if (!$result) {
-                    $output["flag"] = "insert failed";
+                    $output["flag"] = "failed to create event";
                     // return (json_encode($output));
                 }
+                if(isset($_POST["invitedUsers"])){
+                    $query = "SELECT id From event WHERE event.event_date = '$date' and event.start_time = '$s_time'";
+                    $event_s_res = mysqli_query($dblink,$query) or die (mysqli_error($dblink));
+
+                    if(!$event_s_res)
+                    {
+                        $output["flag"] = "failed";
+                        $output["msg"] = "Event id not found";
+                    }
+                    else{
+                        /*$no_of_rows = mysqli_num_rows($event_s_res);
+                        if ($no_of_rows < 1)
+                            $output["event"]="event not found";   //user not found
+                        else {
+                            $output["flag"] = "user found";
+                            $output["users"] = array();
+                            $row = mysqli_fetch_assoc($result);
+                            $output["row"]= $row;
+                        }
+                        $query = "insert into attending (event_id,user_mobile,status)VALUES";
+                        // this is where the magic happens
+                        $array = json_decode($_POST["invitedUsers"],true);
+                        $it = new ArrayIterator( $array );
+                        $output["invited_users"]=$array;
+
+                        // a new caching iterator gives us access to hasNext()
+                        $cit = new CachingIterator( $it );
+                        $status = "deny";
+                        // loop over the array
+                        foreach ( $cit as $value )
+                        {
+                            $cit->
+                            // add to the query
+                            $query .= "('".$row["id"]."','" .$cit->current()."','".$status."')";
+                            // if there is another array member, add a comma
+                            if( $cit->hasNext() )
+                            {
+                                $query .= ",";
+                            }
+                        }
+                        $insert_u_res = mysqli_query($dblink,$query) or die (mysqli_error($dblink));
+                        if(!$insert_u_res){
+                            $output["flag"] = "failed to insert invited users";
+                            $output["msg"] = $insert_u_res;
+                        }
+                        */
+                    }
+                }
+
+
             }
             else {
 
