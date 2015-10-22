da35fa4845bcc3a88b356bc8c15cefe9e654b2d5
matant <matant86@gmail.com>
2015-10-22 11:36:03 +0300

    update  22/10- invite users: -insert new event to attending table-working

--- create_event.php
+++ create_event.php
@@ -48,8 +48,8 @@ class CreateEvent implements ResponseProcess {
                 $output["flag"] = "success";
                 $output["msg"] = "insert event";
 
-                $result = mysqli_query($dblink, "INSERT into event(kind_of_sport,event_date,start_time,end_time,longtitude,latitude,private,gender,min_age,max_participants,scheduled,event_status)
-             VALUES ('$sport','$date','$s_time','$e_time','$lon','$lat','$event_type','$gen','$min_age','$max_p','$sched','1')") or die (mysqli_error($dblink));
+                $result = mysqli_query($dblink, "INSERT into event(kind_of_sport,event_date,start_time,end_time,longtitude,latitude,private,gender,min_age,max_participants,,current_participants,scheduled,event_status)
+             VALUES ('$sport','$date','$s_time','$e_time','$lon','$lat','$event_type','$gen','$min_age','$max_p','1','$sched','1')") or die (mysqli_error($dblink));
                 if (!$result) {
                     $output["flag"] = "failed to create event";
                     // return (json_encode($output));
