27d815288339ce581e918f2a7023395b8aef3180
matant <matant86@gmail.com>
2015-10-27 12:58:11 +0200

    update  27/10-  begin to work on  Google Cloud Messages

--- register.php
+++ register.php
@@ -5,13 +5,10 @@
  * Date: 16/08/2015
  * Time: 17:52
  */
-
 include 'response_process.php';
 require_once 'PasswordFunctions.php';
 class Register implements ResponseProcess{
-
     public function dataProcess($dblink) {
-
         $name = $_POST['firstname'];
         $email = $_POST['email'];//send the email as userName
         $pass = ($_POST['password']);
@@ -19,49 +16,39 @@ class Register implements ResponseProcess{
         $birth = $_POST['birthyear'];
         $gen = $_POST['gender'];
         $pic = $_POST['picture'];
-
         $passFunc = new PasswordFunctions();
-
         $salt = $passFunc->random_password();
         $pass = $passFunc->encrypt($pass, $salt);
-
-
         $imageName = $mob.".jpg";
         $filePath = "images/".$imageName;
         if(file_exists($filePath))
         {
             unlink($filePath); // delete the old file
         }
-
         //create a new empty file
         $myfile =  fopen($filePath,"w") or die("uUnable to open file!");
         file_put_contents($filePath,base64_decode($pic));
-
         $output = array();
-
         $result1 = mysqli_query($dblink,"SELECT * FROM users WHERE users.email= '$email'");
         $result2 = mysqli_query($dblink,"SELECT * FROM users WHERE users.mobile= '$mob'");
-
         if((!$result1) || (!$result2)){
             $output["error_msg"] = "signup query failed";
             print(json_encode($output));
         }
-
         $no_of_rows1 = mysqli_num_rows($result1);
         $no_of_rows2 = mysqli_num_rows($result2);
-
         if ($no_of_rows1 == 1) {
             $output["usercheck"] = "user already exists";       //user already exists
             $output["flag"] = "wrong input";
-            }
+        }
         else if ($no_of_rows2 == 1){
             $output["mobilecheck"] = "mobile already exists";   //mobile already registered
             $output["flag"] = "wrong input";
-            }
+        }
         else
         {
             $userStatus = 0;
-              //user registered
+            //user registered
             //insert user details to DB
             $insertResult=mysqli_query($dblink,"INSERT INTO users (name, email, gender, age, password, salt, userstatus, image,mobile) VALUES
                ('$name', '$email', '$gen', '$birth', '$pass','$salt','$userStatus','$imageName','$mob')") or die((mysqli_error($dblink)));
@@ -75,8 +62,6 @@ class Register implements ResponseProcess{
                 $output["usecase"]="register";
             }
         }
-
         return json_encode($output);
     }
-
 }
