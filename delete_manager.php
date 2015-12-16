dd11ab03a9781a5611dadf270341360ccf269ffe
NirBercovic <nirbercovic@gmail.com>
2015-12-16 22:25:19 +0200

    Delete Manager

--- delete_manager.php
+++ delete_manager.php
@@ -40,6 +40,7 @@ class delete_manager implements ResponseProcess {
                     $output["flag"] = "success";
                     $output["msg"] = $result_q;
                     $no_of_rows = mysqli_num_rows($result_q);
+                    //if number of candidates for manager gt than 0
                     if ($no_of_rows > 0) {
                         $row = mysqli_fetch_assoc($result_q);
                         $user_id = $row["user_id"];
@@ -51,23 +52,58 @@ class delete_manager implements ResponseProcess {
                             $output["affected row"] = $affected_row;
 
                         } else {
-                            $result_q = $dbF->DeleteUserFromAttending($event_id, $user_id);
-                            if (!$result_q) {
+                            $result_status = $dbF->DeleteUserFromAttending($event_id, $user_id);
+                            if (!$result_status) {
                                 $output["flag"] = "failed";
-                                $output["msg"] = $result_q;
+                                $output["msg"] = $result_status;
                             } else {
                                 if ($event_is_private == "true") {
-                                    $users = array();
-                                    $users[] = $userid;
-                                    $result_q = $dbF->InsertIntoAttendingUpdatedUsers($users, $event_id, count($users));
-                                    if (!$result_q) {
+                                    $update_res = $dbF->updateEventUsersCounting($event_id, "current_participants", "dec");
+                                    if (!$update_res) {
                                         $output["flag"] = "failed";
                                         $output["msg"] = $result_q;
                                     } else {
-                                        $output["flag"] = "success";
-                                        $output["msg"] = "deleted user from event";
+                                        $users = array();
+                                        $users[] = $userid;
+                                        $result_q = $dbF->InsertIntoAttendingUpdatedUsers($users, $event_id, count($users));
+                                        if (!$result_q) {
+                                            $output["flag"] = "failed";
+                                            $output["msg"] = $result_q;
+                                        } else {
+                                            $output["flag"] = "success";
+                                            $output["msg"] = "deleted user from event";
+                                        }
                                     }
-
+                                }
+                                else
+                                {   if ($result_status == waiting) {
+                                        $update_res = $dbF->updateEventUsersCounting($event_id, "current_waiting", "dec");
+                                        if (!$update_res) {
+                                            $output["flag"] = "failed";
+                                            $output["msg"] = $update_res;
+                                        } else
+                                            $output["flag"] = "success";
+                                    }
+                                    else
+                                    {//if you delete user who were participating and was the last to get in the playing list - check if there's someone waiting to play
+                                        // if so change his status and just dec the current waiting list count
+                                        $change_status = $dbF->ChangeStatusForAWaitingUser($event_id);
+                                        if(!$change_status) {
+                                            $output["flag"] = "failed";
+                                            $output["msg"] = $change_status;
+                                        }
+                                        elseif ($change_status == "changed")
+                                            $update_res = $dbF->updateEventUsersCounting($event_id, "current_waiting", "dec");
+                                        else //else just dec the current participants list count // regular case
+                                            $update_res = $dbF->updateEventUsersCounting($event_id, "current_participants", "dec");
+                                        if(!$update_res)
+                                        {
+                                            $output["flag"]= "failed";
+                                            $output["msg"] = $update_res;
+                                        }
+                                        else
+                                            $output["flag"]= "success";
+                                    }
                                 }
                             }
                         }
